1cfa5c42a8f45bc1a972808d268cd38f03e19e86
cboelter <christopher@boelter.eu>
2015-06-05 11:45:36 +0200

    fixes

--- module/config/contao-bootstrap.php
+++ module/config/contao-bootstrap.php
@@ -1,15 +1,9 @@
 <?php
 
 return array(
-    'config'   => array(
-        'types' => array(
-            'icons_set' => 'Netzmacht\Bootstrap\Core\Config\Type\IconSetType',
-            'dropdown'  => 'Netzmacht\Bootstrap\Core\Config\Type\DropdownType'
-        ),
-    ),
     'templates' => array(
         'modifiers' => array(
-            'callback_replace-image-classes' => array(
+            'callback_replace-revision6-image-classes' => array(
                 'type'      => 'callback',
                 'callback'  => array('Revision6\Bootstrap\Templates\Modifier', 'replaceImageClasses'),
                 'templates' => array('ce_*'),
